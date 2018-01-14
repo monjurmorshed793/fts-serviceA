036f1c59ca895c52c36193d2cde1de96ce5995b5
monjurmorshed793 <monjurmorshed793@gmail.com>
2018-01-14 15:49:43 +0600

    bug resolved.

--- src/test/java/org/ums/servicea/ServiceaApplicationTests.java
+++ src/test/java/org/ums/servicea/ServiceaApplicationTests.java
@@ -15,38 +15,40 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 @SpringBootTest
 public class ServiceaApplicationTests {
 
-    @Autowired
-    private KafkaTemplate<String, String> template;
+  @Autowired
+  private KafkaTemplate<String, String> template;
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
-	@Test
-    public void testKafkaMessage() throws Exception{
-	    //this.template.send("my_topic", "key2", "message_to_serviceB");
-        ServiceStatus serviceStatus = new ServiceStatus();
-        serviceStatus.setServiceId("serviceA");
-        serviceStatus.setParentServiceId(null);
-        serviceStatus.setStatus(false);
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonToString=mapper.writeValueAsString(serviceStatus);
-        this.template.send("my_topic", "serviceB", jsonToString);
-        /*this.template.send("tracker", "serviceA", jsonToString);
-        assertThat(getSuccessResponse()).isEqualTo(true);*/
+  @Test
+  public void testKafkaMessage() throws Exception {
+    //this.template.send("my_topic", "key2", "message_to_serviceB");
+    ServiceStatus serviceStatus = new ServiceStatus();
+    serviceStatus.setServiceId("serviceA");
+    serviceStatus.setParentServiceId(null);
+    serviceStatus.setStatus(false);
+    ObjectMapper mapper = new ObjectMapper();
+    String jsonToString = mapper.writeValueAsString(serviceStatus);
+    this.template.send("my_topic", "serviceB", jsonToString);
+       /* this.template.send("my_topic", "serviceB", jsonToString);
+    this.template.send("tracker", "serviceA", jsonToString);*/
+    this.template.send("tracker", "serviceA", jsonToString);
+    assertThat(getSuccessResponse()).isEqualTo(true);
+  }
+
+  private boolean getSuccessResponse() throws InterruptedException {
+    Boolean serviceExecutionStatus = false;
+    RestTemplate restTemplate = new RestTemplate();
+    for (int i = 0; i < 15; i++) {
+      serviceExecutionStatus = restTemplate.getForObject("http://localhost:8099/service?service-name=serviceA&service-number=1", Boolean.class);
+      if (serviceExecutionStatus)
+        break;
+      Thread.sleep(500);
     }
-
-    private boolean getSuccessResponse() throws InterruptedException {
-        Boolean serviceExecutionStatus=false;
-        RestTemplate restTemplate = new RestTemplate();
-        for(int i=0; i<15; i++){
-            serviceExecutionStatus=restTemplate.getForObject("http://localhost:8099/service?service-name=serviceA&service-number=1", Boolean.class);
-            if(serviceExecutionStatus)
-                break;
-            Thread.sleep(500);
-        }
-        return serviceExecutionStatus;
-    }
+    return serviceExecutionStatus;
+  }
 
 
 }
