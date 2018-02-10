0d9ff6f0749ff7c0abd89908b42141f075bf9537
monju <monjurmorshed793@gmail.com>
2018-02-11 01:24:42 +0600

    Parallel process added.

--- src/main/java/org/ums/servicea/ServiceaApplication.java
+++ src/main/java/org/ums/servicea/ServiceaApplication.java
@@ -1,19 +1,96 @@
 package org.ums.servicea;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.RequestEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.io.InputStream;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 @RestController("/")
 public class ServiceaApplication {
 
+  @Autowired
+  private KafkaTemplate<String, String> template;
+
     @GetMapping("/")
     public String getHome(RequestEntity requestEntity){
         return "Server A";
     }
+
+    //@PostConstruct
+  public void testSynchronizeRequestCall(){
+    for(int i=1;i<=100; i++){
+      RestTemplate restTemplate = new RestTemplate();
+      long startTime = System.nanoTime();
+      Boolean status = restTemplate.getForObject("http://localhost:8082/service-b?number="+i, Boolean.class);
+      if(status){
+        System.out.print("Call--->"+i+" & time taken: "+((System.nanoTime()-startTime)/1000000)/1000 +"\n");
+      }
+    }
+  }
+
+  //@PostConstruct
+  public void parallelCall(){
+  System.out.println("In the parallel call");
+    IntStream.range(0,100).parallel().forEach(i->{
+      RestTemplate restTemplate = new RestTemplate();
+      long startTime = System.nanoTime();
+      Boolean status = restTemplate.getForObject("http://localhost:8082/service-b/parallel?number="+i, Boolean.class);
+      if(status){
+        System.out.print("Call--->"+i+" & time taken: "+((System.nanoTime()-startTime)/1000000)/1000 +"\n");
+      }
+    });
+  }
+
+  public void parallelMessageQCall() {
+    IntStream.range(0,100).sequential().forEach(i->{
+      ServiceStatus serviceStatus = new ServiceStatus();
+      serviceStatus.setServiceId("serviceA"+i);
+      serviceStatus.setParentServiceId(null);
+      serviceStatus.setStatus(false);
+      ObjectMapper mapper = new ObjectMapper();
+      String jsonToString="";
+      long startTime = System.nanoTime();
+      try{
+        jsonToString = mapper.writeValueAsString(serviceStatus);
+        this.template.send("service-b", i+"", jsonToString);
+       /* this.template.send("my_topic", "serviceB", jsonToString);
+    this.template.send("tracker", "serviceA", jsonToString);*/
+        this.template.send("tracker", "serviceA", jsonToString);
+        if(getSuccessResponse()==false)
+          throw new NullPointerException();
+      }catch (Exception e){
+        e.printStackTrace();
+      }
+
+      System.out.print("Call--->"+i+" & time taken: "+((System.nanoTime()-startTime)/1000000)/1000 +"\n");
+
+
+    });
+  }
+
+
+  private boolean getSuccessResponse() throws InterruptedException {
+    Boolean serviceExecutionStatus = false;
+    RestTemplate restTemplate = new RestTemplate();
+    for (int i = 0; i < 15; i++) {
+      serviceExecutionStatus = restTemplate.getForObject("http://localhost:8099/service?service-name=serviceA&service-number=1", Boolean.class);
+      if (serviceExecutionStatus)
+        break;
+      Thread.sleep(500);
+    }
+    return serviceExecutionStatus;
+  }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceaApplication.class, args);
--- src/test/java/org/ums/servicea/ServiceaApplicationTests.java
+++ src/test/java/org/ums/servicea/ServiceaApplicationTests.java
@@ -1,6 +1,7 @@
 package org.ums.servicea;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
+
+import java.sql.Timestamp;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
@@ -23,26 +26,41 @@ public class ServiceaApplicationTests {
   }
 
   @Test
-  public void testKafkaMessage() throws Exception {
-    //this.template.send("my_topic", "key2", "message_to_serviceB");
-    ServiceStatus serviceStatus = new ServiceStatus();
-    serviceStatus.setServiceId("serviceA");
-    serviceStatus.setParentServiceId(null);
-    serviceStatus.setStatus(false);
-    ObjectMapper mapper = new ObjectMapper();
-    String jsonToString = mapper.writeValueAsString(serviceStatus);
-    this.template.send("my_topic", "serviceB", jsonToString);
-       /* this.template.send("my_topic", "serviceB", jsonToString);
-    this.template.send("tracker", "serviceA", jsonToString);*/
-    this.template.send("tracker", "serviceA", jsonToString);
-    assertThat(getSuccessResponse()).isEqualTo(true);
+  public void testSynchronizeRequestCall(){
+    for(int i=1;i<=100; i++){
+      RestTemplate restTemplate = new RestTemplate();
+      long startTime = System.nanoTime();
+      Boolean status = restTemplate.getForObject("http://localhost:8082/service-b?number="+i, Boolean.class);
+      if(status){
+        System.out.print("Call--->"+i+" & time taken: "+(System.nanoTime()-startTime));
+      }
+    }
   }
 
-  private boolean getSuccessResponse() throws InterruptedException {
+  @Test
+  public void testKafkaMessage() throws Exception {
+    //this.template.send("my_topic", "key2", "message_to_serviceB");
+    for(int i=1;i<=3;i++){
+      ServiceStatus serviceStatus = new ServiceStatus();
+      serviceStatus.setServiceId("serviceA"+i);
+      serviceStatus.setParentServiceId(null);
+      serviceStatus.setStatus(false);
+      ObjectMapper mapper = new ObjectMapper();
+      String jsonToString = mapper.writeValueAsString(serviceStatus);
+      this.template.send("service-b", ""+i, jsonToString);
+       /* this.template.send("my_topic", "serviceB", jsonToString);
+    this.template.send("tracker", "serviceA", jsonToString);*/
+      this.template.send("tracker", "serviceA", jsonToString);
+      assertThat(getSuccessResponse(serviceStatus.getServiceId())).isEqualTo(true);
+    }
+
+  }
+
+  private boolean getSuccessResponse(String serviceName) throws InterruptedException {
     Boolean serviceExecutionStatus = false;
     RestTemplate restTemplate = new RestTemplate();
-    for (int i = 0; i < 15; i++) {
-      serviceExecutionStatus = restTemplate.getForObject("http://localhost:8099/service?service-name=serviceA&service-number=1", Boolean.class);
+    for (int i = 0; i < 50; i++) {
+      serviceExecutionStatus = restTemplate.getForObject("http://localhost:8099/service?service-name="+serviceName+"&service-number=1", Boolean.class);
       if (serviceExecutionStatus)
         break;
       Thread.sleep(500);
